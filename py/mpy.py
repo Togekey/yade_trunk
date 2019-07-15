5d85a87abdcdb61c4d757d7ee17f5bbb97278ff4
dpkn31 <deepak.kn1990@gmail.com>
2019-07-15 19:24:24 +0200

    option to split mpi communicator

--- py/mpy.py
+++ py/mpy.py
@@ -34,13 +34,19 @@ from mpi4py import MPI
 import numpy as np
 import yade.bisectionDecomposition as dd
 
+
+
 sys.stderr.write=sys.stdout.write #so we see error messages from workers
 
 this = sys.modules[__name__]
 
-comm = MPI.COMM_WORLD
+worldComm = MPI.COMM_WORLD
+color = 1 ; key = 0 ; 
+comm = worldComm.Split(color, key)
 rank = comm.Get_rank()
 numThreads = comm.Get_size()
+commSplit = False 
+
 
 waitingCommands=False #are workers currently interactive?
 userScriptInCheckList=""	# the simulation script from which mpy.py is used
@@ -64,6 +70,7 @@ NUM_MERGES = 0
 SEND_BYTEARRAYS = True
 ENABLE_PFACETS = False    #PFacets need special (and expensive) tricks, if PFacets are not used skip the tricks
 DISTRIBUTED_INSERT = False  #if True each worker is supposed to "O.bodies.insertAtId" its own bodies 
+
 
 
 #tags for mpi messages
@@ -137,16 +144,23 @@ def initialize():
 			#TODO: if process_count>numThreads, free some workers
 			rank=0
 		else:	#WORKERS
-			comm = MPI.Comm.Get_parent().Merge()
-			rank=comm.Get_rank()
+			if not commSplit:
+				comm = MPI.Comm.Get_parent().Merge()
+				rank=comm.Get_rank()
+			else: 
+				rank = comm.Get_rank()
 			mprint("Hello, I'm worker "+str(rank))
 	else:
-		rank = os.getenv('OMPI_COMM_WORLD_RANK')
-		numThreads=None
-		if rank is not None: #mpiexec was used
-			rank=int(rank)
-			numThreads=int(os.getenv('OMPI_COMM_WORLD_SIZE'))
-		#else monolithic simulation (no mpiexec, no mit)
+		if not commSplit:
+			rank = os.getenv('OMPI_COMM_WORLD_RANK')
+			numThreads=None
+			if rank is not None: #mpiexec was used
+				rank=int(rank)
+				numThreads=int(os.getenv('OMPI_COMM_WORLD_SIZE'))
+			#else monolithic simulation (no mpiexec, no mit)
+		else: 
+			rank = comm.Get_rank() 
+			numThreads = comm.Get_size()
 	return rank,numThreads
 
 def autoInitialize(np):
