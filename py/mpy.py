e87dfbb319720585a110e78f0cc960ff26b76f61
dpkn31 <deepak.kn1990@gmail.com>
2019-07-29 17:37:45 +0200

    set the subdomain ids to O.subdomainIds, and O.thisSubdomainId

--- py/mpy.py
+++ py/mpy.py
@@ -789,7 +789,7 @@ def splitScene():
 		
 		# insert force communicator before Newton
 		O.engines=O.engines[:idx]+[PyRunner(iterPeriod=1,initRun=True,command="sys.modules['yade.mpy'].isendRecvForces()",label="isendRecvForcesRunner")]+O.engines[idx:]
-		
+
 		# append engine waiting until forces are effectively sent to master
 		O.engines=O.engines+[PyRunner(iterPeriod=1,initRun=True,command="pass",label="waitForcesRunner")]
 		O.engines=O.engines+[PyRunner(iterPeriod=1,initRun=True,command="if sys.modules['yade.mpy'].checkAndCollide(): O.pause();",label="collisionChecker")]
