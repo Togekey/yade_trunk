f4a8a7ccdd21f6d64ed4eca43ec349a9d0edf947
dpkn31 <deepak.kn1990@gmail.com>
2019-07-29 17:36:58 +0200

    get subdomain ids from scene, and id of the subdomain of the current subdomain from scene

--- py/mpy.py
+++ py/mpy.py
@@ -1030,7 +1030,6 @@ def mpirun(nSteps,np=None,withMerge=False):
 			
 	# split if needed
 	initStep = O.iter
-	
 	if not O.splitted:
 		wprint("splitting")
 		splitScene()
@@ -1052,7 +1051,9 @@ def mpirun(nSteps,np=None,withMerge=False):
 		mergeScene()
 	
 	# report performance
+
 	if YADE_TIMING and rank<=MAX_RANK_OUTPUT:
+		timing_comm.print_all()
 		from yade import timing
 		time.sleep((numThreads-rank)*0.002) #avoid mixing the final output, timing.stats() is independent of the sleep
 		mprint( "#####  Worker "+str(rank)+"  ######")
