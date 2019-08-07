da9baaf1b0b5bf84c6b925e20e025c01486001ac
Janek Kozicki <janek_listy@wp.pl>
2019-08-07 13:21:52 +0200

    Use R"""( …… )""" https://en.cppreference.com/w/cpp/language/string_literal so it is easier to write more about issue 106

--- pkg/common/Collider.hpp
+++ pkg/common/Collider.hpp
@@ -45,7 +45,18 @@ class Collider: public GlobalEngine {
 	YADE_CLASS_BASE_DOC_ATTRS_CTOR_PY(Collider,GlobalEngine,"Abstract class for finding spatial collisions between bodies. \n\n.. admonition:: Special constructor\n\n\tDerived colliders (unless they override ``pyHandleCustomCtorArgs``) can be given list of :yref:`BoundFunctors <BoundFunctor>` which is used to initialize the internal :yref:`boundDispatcher <Collider.boundDispatcher>` instance.",
 		((shared_ptr<BoundDispatcher>,boundDispatcher,new BoundDispatcher,Attr::readonly,":yref:`BoundDispatcher` object that is used for creating :yref:`bounds <Body.bound>` on collider's request as necessary.")),
 		/*ctor*/,
-		.add_property("avoidSelfInteractionMask",&Collider::get_avoidSelfInteractionMask,&Collider::set_avoidSelfInteractionMask,"This mask is used to avoid the interactions inside a group of particles. To do so, the particles must have the exact same mask and that mask should have one bit in common with this :yref:`avoidSelfInteractionMask<Collider.avoidSelfInteractionMask>` as for their binary representations.")
+		.add_property("avoidSelfInteractionMask",&Collider::get_avoidSelfInteractionMask,&Collider::set_avoidSelfInteractionMask , R"""(
+This mask is used to avoid the interactions inside a group of particles. To do so, the particles must have the exact same mask and that mask should have one bit in common with this :yref:`avoidSelfInteractionMask<Collider.avoidSelfInteractionMask>` as for their binary representations.
+
+In an example with 3 mask values assigned to particles: ``0b10`` = "blue", ``0b01`` = "green", ``0b11`` = "red", and a with collider.avoidSelfInteractionMask = ``0b01``:
+
+* Green particles would not interact with blue particles because they are not mask-compatible (do not share any "1" bit in common): ``0b01 & 0b10`` is false
+* Green particles would not interact with other green particles because of avoidSelfInteractionMask: ``0b01 & 0b01`` is true
+* Red particles would not interact with themselves either: ``0b11 & 0b01`` is true
+* For the same reason blue particles would interact with themselves because for them avoidSelfInteractionMask: ``0b10 and 0b01`` is false
+
+For the complete list of possibilities, see :ysrc:`pkg/common/Collider.cpp#L15`.
+	)""")
 	);
 };
 REGISTER_SERIALIZABLE(Collider);
