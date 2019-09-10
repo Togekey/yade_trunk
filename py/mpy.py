ab325b7df580314c232c8618b9d68e4a030bcffa
dpkn31 <deepak.kn1990@gmail.com>
2019-09-10 19:04:01 +0200

    some minor fixes

--- py/mpy.py
+++ py/mpy.py
@@ -422,9 +422,6 @@ def unboundRemoteBodies():
 	'''
 	for b in O.bodies:# unbound the bodies assigned to workers (not interacting directly with other bodies in master scene)
 		if (not (b.isSubdomain or isinstance(b.shape, FluidDomainBbox)) and b.subdomain!=rank):
-		
-			#if b: 
-			#	if (isinstance(b.shape,FluidDomainBbox)):continue 
 			b.bounded=False
 			
 def reboundRemoteBodies(ids):
@@ -535,7 +532,7 @@ def genLocalIntersections(subdomains):
 			b=O.bodies[otherId]
 			if not b:continue #in case the body was deleted
 			if b.subdomain!=rank: continue
-			if b.isSubdomain: intersections[rank].append(subdIdx) #intersecting subdomain (will need to receive updated positions from there)
+			if b.isSubdomain : intersections[rank].append(subdIdx) #intersecting subdomain (will need to receive updated positions from there)
 			else:
 				if isinstance(b.shape,PFacet):
 						intersections[subdIdx]+= maskedPFacet(b, appended); continue
@@ -794,8 +791,6 @@ def splitScene():
 			fluidCoupling.comm = comm 
 			fluidCoupling.getFluidDomainBbox() #triggers the communication between yade procs and Yales2/openfoam procs, get's fluid domain bounding boxes from all yales2 procs. 
 			fluidCoupling.setIdList(fluidBodies) 
-			
-		
 		parallelCollide()
 		
 		# insert states communicator after newton 
