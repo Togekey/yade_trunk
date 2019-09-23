e964c03180e807c1e99bd05b012dc910610ffe21
bchareyre <bruno.chareyre@grenoble-inp.fr>
2019-09-23 21:25:42 +0200

    MPI: keep niter the same in both parallel and serial execution of the test script

--- examples/mpi/testMPI_2D.py
+++ examples/mpi/testMPI_2D.py
@@ -64,7 +64,6 @@ for sd in range(0,numThreads-1):
 WALL_ID=O.bodies.append(box(center=(numThreads*N*0.5,-0.5,0),extents=(2*numThreads*N,0,2),fixed=True))
 
 collider.verletDist = 0.5
-collider.keepListsShort=True
 newton.gravity=(0,-10,0) #else nothing would move
 tsIdx=O.engines.index(timeStepper) #remove the automatic timestepper. Very important: we don't want subdomains to use many different timesteps...
 O.engines=O.engines[0:tsIdx]+O.engines[tsIdx+1:]
@@ -84,7 +83,7 @@ def collectTiming():
 
 if rank is None: #######  Single-core  ######
 	O.timingEnabled=True
-	O.run(NSTEPS,True)
+	O.run(NSTEPS+1,True)
 	#print "num bodies:",len(O.bodies)
 	from yade import timing
 	timing.stats()
