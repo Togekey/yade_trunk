795cf82a008d7befcca36371e2898d7e12b889f7
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2019-10-25 19:30:32 +0200

    Prepare simple GUI check for mpmath

--- pkg/common/Sphere.hpp
+++ pkg/common/Sphere.hpp
@@ -1,6 +1,17 @@
 #pragma once
 #include<core/Shape.hpp>
 
+// FIXME - move these headers into single place. Current duplicates: customConverters, _testCppPy
+#if YADE_REAL_BIT>=128
+#include <boost/cstdfloat.hpp>
+#include <boost/math/cstdfloat/cstdfloat_types.hpp>
+#include <boost/multiprecision/float128.hpp>
+#endif
+
+#ifdef YADE_MPFR
+#include <boost/multiprecision/mpfr.hpp>
+#endif
+
 // FIXED: add namespace yade, see https://gitlab.com/yade-dev/trunk/issues/57 and (old site, fixed bug) https://bugs.launchpad.net/yade/+bug/528509
 
 namespace yade {
@@ -11,7 +22,9 @@ class Sphere: public Shape{
 		virtual ~Sphere () {};
 	// clang-format off
 	YADE_CLASS_BASE_DOC_ATTRS_CTOR(Sphere,Shape,"Geometry of spherical particle.",
-		((Real,radius,NaN,,"Radius [m]")),
+		((Real,radius,NaN,,"Radius [m]"))
+		((boost::float128_t,test128,NaN,,"Test 128"))
+		,
 		createIndex(); /*ctor*/
 	);
 	// clang-format on
