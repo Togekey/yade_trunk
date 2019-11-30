e949f7a0f7dc2862ba6ec663f674a53d50ccaaf8
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2019-11-30 14:47:50 +0100

    Add namespace yade::bmp=::boost::multiprecision;

--- lib/base/AliasNamespaces.hpp
+++ lib/base/AliasNamespaces.hpp
@@ -11,12 +11,17 @@
 // don't include here <boost/filesystem.hpp> or <Python.h>. Better to include them in places where they are needed.
 
 namespace boost {
-	namespace python {}
-	namespace filesystem {}
+namespace python {
+}
+namespace filesystem {
+}
+namespace multiprecision {
+}
 }
 
 namespace yade {
-	namespace py =::boost::python;
-	namespace bfs=::boost::filesystem;
+namespace py  = ::boost::python;
+namespace bfs = ::boost::filesystem;
+namespace bmp = ::boost::multiprecision;
 }
 
