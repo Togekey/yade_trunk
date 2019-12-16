261e2a412bc2b0f42efcf572b935a8dee1c35e1d
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2019-12-16 15:27:27 +0100

    Cleanup in MathFunctions.hpp

--- lib/high-precision/MathFunctions.hpp
+++ lib/high-precision/MathFunctions.hpp
@@ -5,11 +5,12 @@
 *  GNU General Public License v2 or later. See file LICENSE for details. *
 *************************************************************************/
 
-// This file conaints mathematical functions available in standard library and boost library.
+// This file contains mathematical functions available in standard library and boost library.
 //     https://en.cppreference.com/w/cpp/numeric/math
 //     https://en.cppreference.com/w/cpp/numeric/special_functions
+// They have to be provided here as inline redirections towards the correct implementation, depending on what precision type yade is being compiled with.
 
-// TODO: Boost documentation recommends to link with tr1: -lboost_math_tr1 as it provides significand speedup. For example replace boost::math::acosh(x) ↔ boost::math::tr1::acosh(x)
+// TODO: Boost documentation recommends to link with tr1: -lboost_math_tr1 as it provides significant speedup. For example replace boost::math::acosh(x) ↔ boost::math::tr1::acosh(x)
 //     https://www.boost.org/doc/libs/1_71_0/libs/math/doc/html/math_toolkit/overview_tr1.html
 //#include <boost/math/tr1.hpp>
 
@@ -30,7 +31,7 @@
 #error "This file cannot be included alone, include Real.hpp instead"
 #endif
 
-// Macors for quick inline redirections towards the correct function from standard library or boost::multiprecision
+// Macors for quick inline redirections towards the correct function from (1) standard library or (2) boost::multiprecision; depending on which one is used.
 #define YADE_WRAP_FUNC_1(func)                                                                                                                                 \
 	inline Real func(const Real& a) { return YADE_REAL_MATH_NAMESPACE::func(static_cast<UnderlyingReal>(a)); }
 
@@ -81,73 +82,99 @@
 
 namespace yade {
 
-#ifdef YADE_THIN_REAL_WRAPPER_HPP
-// Some old C library functions need pointer to C-array, this is for compatibility between ThinRealWrapper and UnderlyingReal
-static_assert(sizeof(Real)==sizeof(UnderlyingReal),"This compiler introduced padding, which breaks binary compatibility");
+/********************************************************************************************/
+/**********************            trigonometric functions             **********************/
+/********************************************************************************************/
 
-static inline const UnderlyingReal* constVectorData(const std::vector<Real>& v) { return v.data()->operator const UnderlyingReal*(); }
-static inline UnderlyingReal*       vectorData(std::vector<Real>& v) { return v.data()->operator UnderlyingReal*(); }
-
-#else
-
-static inline const UnderlyingReal* constVectorData(const std::vector<Real>& v) { return v.data(); }
-static inline UnderlyingReal*       vectorData(std::vector<Real>& v) { return v.data(); }
-
-#endif
-
-// These random functions are necessary for Eigen library to for example write in python: Vector3.Random()
-// random number [0,1)
-static inline Real random01()
-{
-#if defined(YADE_REAL_MPFR_NO_BOOST_experiments_only_never_use_this)
-	return ::mpfr::random();
-#else
-	static ::boost::random::mt19937 gen;
-	return ::boost::random::generate_canonical<Real, std::numeric_limits<Real>::digits>(gen);
-#endif
-}
-
-static inline Real unitRandom() { return random01(); }
-static inline Real random() { return random01() * 2 - 1; }
-
-template <typename T> int sgn(T val) { return (T(0) < val) - (val < T(0)); }
-template <typename T> int sign(T val) { return (T(0) < val) - (val < T(0)); }
-
-YADE_WRAP_FUNC_1(abs)
-YADE_WRAP_FUNC_1(acos)
-YADE_WRAP_FUNC_1(acosh)
-YADE_WRAP_FUNC_1(asin)
-YADE_WRAP_FUNC_1(asinh)
-YADE_WRAP_FUNC_1(atan)
-YADE_WRAP_FUNC_1(atanh)
-YADE_WRAP_FUNC_1(cbrt)
-YADE_WRAP_FUNC_1(ceil)
+YADE_WRAP_FUNC_1(sin)
+YADE_WRAP_FUNC_1(sinh)
 YADE_WRAP_FUNC_1(cos)
 YADE_WRAP_FUNC_1(cosh)
-YADE_WRAP_FUNC_1(erf)
-YADE_WRAP_FUNC_1(erfc)
-YADE_WRAP_FUNC_1(exp)
-YADE_WRAP_FUNC_1(exp2)
-YADE_WRAP_FUNC_1(expm1)
-YADE_WRAP_FUNC_1(floor)
-YADE_WRAP_FUNC_1(ilogb)
-YADE_WRAP_FUNC_1(lgamma)
+YADE_WRAP_FUNC_1(tan)
+YADE_WRAP_FUNC_1(tanh)
+
+/********************************************************************************************/
+/**********************        inverse trigonometric functions         **********************/
+/********************************************************************************************/
+
+YADE_WRAP_FUNC_1(asin)
+YADE_WRAP_FUNC_1(asinh)
+YADE_WRAP_FUNC_1(acos)
+YADE_WRAP_FUNC_1(acosh)
+YADE_WRAP_FUNC_1(atan)
+YADE_WRAP_FUNC_1(atanh)
+YADE_WRAP_FUNC_2(atan2)
+
+/********************************************************************************************/
+/**********************   logarithm, exponential and power functions   **********************/
+/********************************************************************************************/
+
 YADE_WRAP_FUNC_1(log)
 YADE_WRAP_FUNC_1(log10)
 YADE_WRAP_FUNC_1(log1p)
 YADE_WRAP_FUNC_1(log2)
 YADE_WRAP_FUNC_1(logb)
-//YADE_WRAP_FUNC_1(riemann_zeta) // since C++17
-YADE_WRAP_FUNC_1(rint)
-YADE_WRAP_FUNC_1(round)
-YADE_WRAP_FUNC_1(sin)
-YADE_WRAP_FUNC_1(sinh)
-YADE_WRAP_FUNC_1(sqrt)
-YADE_WRAP_FUNC_1(tan)
-YADE_WRAP_FUNC_1(tanh)
+YADE_WRAP_FUNC_1(ilogb)
 
+YADE_WRAP_FUNC_2_TYPE2(ldexp, int)
+YADE_WRAP_FUNC_2_TYPE2(frexp, int*) // that's original C signature of this function
+
+YADE_WRAP_FUNC_1(exp)
+YADE_WRAP_FUNC_1(exp2)
+YADE_WRAP_FUNC_1(expm1)
+
+YADE_WRAP_FUNC_2(pow)
+YADE_WRAP_FUNC_1(sqrt)
+YADE_WRAP_FUNC_1(cbrt)
+
+YADE_WRAP_FUNC_2(hypot)
+//YADE_WRAP_FUNC_3(hypot) // since C++17, could be very useful for us
+
+/********************************************************************************************/
+/**********************    min, max, abs, sign, floor, ceil, round     **********************/
+/********************************************************************************************/
+
+YADE_WRAP_FUNC_2_STD(max)
+YADE_WRAP_FUNC_2_STD(min)
+YADE_WRAP_FUNC_1(abs)
+YADE_WRAP_FUNC_1_RENAME(fabs, abs)
+template <typename T> int sgn(T val) { return (T(0) < val) - (val < T(0)); }
+template <typename T> int sign(T val) { return (T(0) < val) - (val < T(0)); }
+YADE_WRAP_FUNC_1(floor)
+YADE_WRAP_FUNC_1(ceil)
+YADE_WRAP_FUNC_1(round)
+YADE_WRAP_FUNC_1(rint)
+YADE_WRAP_FUNC_1(trunc)
+
+/********************************************************************************************/
+/**********************        integer division and remainder          **********************/
+/********************************************************************************************/
+
+YADE_WRAP_FUNC_2(fmod)
+YADE_WRAP_FUNC_2(remainder)
+
+#if defined(YADE_REAL_MPFR_NO_BOOST_experiments_only_never_use_this)
+YADE_WRAP_FUNC_2_TYPE2_CAST(modf, Real&, NotUsed)
+#else
+YADE_WRAP_FUNC_2_TYPE2_CAST(modf, Real*, UnderlyingReal*)
+#endif
+
+YADE_WRAP_FUNC_3(fma)
+#if defined(YADE_REAL_MPFR_NO_BOOST_experiments_only_never_use_this)
+YADE_WRAP_FUNC_3_TYPE31(remquo, long*)
+#else
+YADE_WRAP_FUNC_3_TYPE3(remquo, int*)
+#endif
+
+/********************************************************************************************/
+/**********************         special mathematical functions         **********************/
+/********************************************************************************************/
+
+YADE_WRAP_FUNC_1(erf)
+YADE_WRAP_FUNC_1(erfc)
+YADE_WRAP_FUNC_1(lgamma)
+// workaround broken tgamma for boost::float128
 #if (YADE_REAL_BIT <= 128) and (YADE_REAL_BIT > 80)
-// workaround broken tgamma for float128
 static_assert(std::is_same<UnderlyingReal, boost::multiprecision::float128>::value, "Incorrect type, please file a bug report.");
 inline Real tgamma(const Real& a)
 {
@@ -162,36 +189,48 @@ inline Real tgamma(const Real& a)
 YADE_WRAP_FUNC_1(tgamma)
 #endif
 
-YADE_WRAP_FUNC_1(trunc)
-YADE_WRAP_FUNC_1_RENAME(fabs, abs)
-YADE_WRAP_FUNC_2(atan2)
-//YADE_WRAP_FUNC_2(beta)         // since C++17
-//YADE_WRAP_FUNC_2(cyl_bessel_i) // since C++17
-//YADE_WRAP_FUNC_2(cyl_bessel_j) // since C++17
-//YADE_WRAP_FUNC_2(cyl_bessel_k) // since C++17
-YADE_WRAP_FUNC_2(fmod)
-YADE_WRAP_FUNC_2(hypot)
-YADE_WRAP_FUNC_2_STD(max)
-YADE_WRAP_FUNC_2_STD(min)
-YADE_WRAP_FUNC_2(pow)
-YADE_WRAP_FUNC_2(remainder)
+// These will be available in C++17, we could use the ones from boost, if they become necessary.
+//YADE_WRAP_FUNC_1(riemann_zeta)
+//YADE_WRAP_FUNC_2(beta)
+//YADE_WRAP_FUNC_2(cyl_bessel_i)
+//YADE_WRAP_FUNC_2(cyl_bessel_j)
+//YADE_WRAP_FUNC_2(cyl_bessel_k)
+//YADE_WRAP_FUNC_2_TYPE1(sph_bessel, unsigned)
 
-//YADE_WRAP_FUNC_2_TYPE1(sph_bessel, unsigned) // since C++17
-YADE_WRAP_FUNC_2_TYPE2(ldexp, int)
-YADE_WRAP_FUNC_2_TYPE2(frexp, int*) // original C signature
-#if defined(YADE_REAL_MPFR_NO_BOOST_experiments_only_never_use_this)
-YADE_WRAP_FUNC_2_TYPE2_CAST(modf, Real&, NotUsed)
+
+/********************************************************************************************/
+/**********************        extract C-array from std::vector        **********************/
+/********************************************************************************************/
+
+// Some old C library functions need pointer to C-array, this is for compatibility between ThinRealWrapper and UnderlyingReal
+#ifdef YADE_THIN_REAL_WRAPPER_HPP
+static_assert(sizeof(Real) == sizeof(UnderlyingReal), "This compiler introduced padding. This breaks binary compatibility");
+
+static inline const UnderlyingReal* constVectorData(const std::vector<Real>& v) { return v.data()->operator const UnderlyingReal*(); }
+static inline UnderlyingReal*       vectorData(std::vector<Real>& v) { return v.data()->operator UnderlyingReal*(); }
 #else
-YADE_WRAP_FUNC_2_TYPE2_CAST(modf, Real*, UnderlyingReal*)
+static inline const UnderlyingReal* constVectorData(const std::vector<Real>& v) { return v.data(); }
+static inline UnderlyingReal*       vectorData(std::vector<Real>& v) { return v.data(); }
 #endif
 
-YADE_WRAP_FUNC_3(fma)
-//YADE_WRAP_FUNC_3(hypot) // since C++17
+/********************************************************************************************/
+/**********************                     random                     **********************/
+/********************************************************************************************/
+
+// These random functions are necessary for Eigen library to for example write in python: Vector3.Random()
+// generate random number [0,1)
+static inline Real random01()
+{
 #if defined(YADE_REAL_MPFR_NO_BOOST_experiments_only_never_use_this)
-YADE_WRAP_FUNC_3_TYPE31(remquo, long*)
+	return ::mpfr::random();
 #else
-YADE_WRAP_FUNC_3_TYPE3(remquo, int*)
+        static ::boost::random::mt19937 gen;
+        return ::boost::random::generate_canonical<Real, std::numeric_limits<Real>::digits>(gen);
 #endif
+}
+
+static inline Real unitRandom() { return random01(); }
+static inline Real random() { return random01() * 2 - 1; }
 
 }
 
