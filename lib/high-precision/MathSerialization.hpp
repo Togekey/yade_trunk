accda56c859489f862551c1f703c4926fb36fa9b
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2020-01-08 21:41:53 +0100

    small fix in serialization.

--- lib/high-precision/MathSerialization.hpp
+++ lib/high-precision/MathSerialization.hpp
@@ -14,8 +14,7 @@
 #ifndef ALL_MATH_TYPES_SERIALIZATION_HPP
 #define ALL_MATH_TYPES_SERIALIZATION_HPP
 
-// For real type only ThinRealWrapper and boost::multiprecision::mpfr need serialization, other types have native support.
-#ifdef YADE_REAL_MPFR
+#if (YADE_REAL_BIT > 80)
 #include <boost/serialization/split_free.hpp>
 BOOST_SERIALIZATION_SPLIT_FREE(::yade::math::Real);
 BOOST_IS_BITWISE_SERIALIZABLE(::yade::math::Real);
@@ -42,26 +41,30 @@ BOOST_IS_BITWISE_SERIALIZABLE(yade::Matrix6r);
 namespace boost {
 namespace serialization {
 
-#ifdef YADE_REAL_MPFR
+#if (YADE_REAL_BIT > 80)
 	template <class Archive> void save(Archive& ar, const ::yade::math::Real& a, unsigned int)
 	{
 		// FIXME: similar code is in ToFromPythonConverter.hpp, MathSerialization.hpp, MathFunctions.hpp, extract it to single place.
-		std::string s = a.str(0, std::ios::fixed);
-		ar & boost::serialization::make_nvp("value",s);
+		// TODO: maybe we can find a faster method for float128
+                static constexpr auto digs1 = std::numeric_limits<Real>::digits10 + 1;
+		std::ostringstream ss;
+		ss << std::setprecision(digs1) << a;
+		std::string v = ss.str();
+		ar & BOOST_SERIALIZATION_NVP(v);
 	}
 	template <class Archive> void load(Archive& ar, ::yade::math::Real& a, unsigned int)
 	{
-		std::string s{};
-		ar & boost::serialization::make_nvp("value",s);
-		std::stringstream ss{s};
+		std::string v{};
+		ar & BOOST_SERIALIZATION_NVP(v);
+		std::stringstream ss{v};
 		ss >> a;
 	}
 #endif
 #if (YADE_REAL_BIT == 80)
 	template <class Archive> void serialize(Archive& ar, ::yade::math::Real& a, unsigned int)
 	{
-		::yade::math::UnderlyingReal& value = a.operator ::yade::math::UnderlyingReal&();
-		ar & BOOST_SERIALIZATION_NVP(value);
+		::yade::math::UnderlyingReal& v = a.operator ::yade::math::UnderlyingReal&();
+		ar & BOOST_SERIALIZATION_NVP(v);
 	}
 #endif
 
